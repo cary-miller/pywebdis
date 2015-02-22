5307e4dc5d2da4583cc504a27fefb6b04e46965a
cary-miller <miller.cary@gmail.com>
2015-02-22 08:13:38 -0700

    split out useful functions

--- webdis.py
+++ webdis.py
@@ -290,28 +290,8 @@ def test_set_methods():
     return 'ok'
       
 
-def first_mismatch(s1, s2):
-    '''index of first mismatch in two strings.
-    >>> first_mismatch('abcd', 'abcd')
-    >>> first_mismatch('abcd', 'abxy')
-    (2, 'c', 'x')
-    >>> first_mismatch('abcd', 'abc')
-    (3, 'd', '')
-    >>> first_mismatch('abcd', 'abcde')
-    (4, '', 'e')
-    '''
-    for (i,(a,b)) in enumerate(zip(s1, s2)):
-        if a!=b:
-            return (i,a,b)
-    L1 = len(s1)
-    L2 = len(s2)
-    if L1 < L2:
-        return (i+1, '', s2[i+1])
-    if L1 > L2:
-        return (i+1, s1[i+1], '')
-
-
 def print_in_place(msg):
     sys.stdout.write(u"\r\x1b[K"+unicode(msg))
     sys.stdout.flush()
+
 
